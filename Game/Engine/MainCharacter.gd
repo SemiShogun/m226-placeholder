5d4543fdb34e7f6d59cadab6ee0151d0d17d483b
lenny cruzinha <cruzinha.lenny@gmail.com>
2020-12-30 01:15:18 +0100

    implemented basic movement and collision detection... more following up

---
+++ Game/Engine/MainCharacter.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("left"):
+		if position.x > 0:
+			position.x -= 1
+		
+	if Input.is_action_pressed("right"):
+		position.x += 1
+	
