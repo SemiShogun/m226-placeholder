1405f034502e1eb36e57edc0274638891e83e7fe
lenny cruzinha <cruzinha.lenny@gmail.com>
2021-02-07 20:17:06 +0100

    .exe generated

--- Game/Engine/Character.gd
+++ Game/Engine/Character.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var movement = Vector2(0,0) #x,y
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,9 +13,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	movement.x = 0
+	movement.y = 0 
 	
-	var movement = Vector2(0,0)
+	input()
+	set_animation()
+	movement = move_and_slide(movement * 200)
 	
+	
+func input(): 
 	if Input.is_action_pressed("left"):
 		movement.x = -1
 		
@@ -27,5 +33,16 @@ func _process(delta):
 		
 	if Input.is_action_pressed("down"):
 		movement.y = 1
-		
-	move_and_slide(movement * 100)
+
+
+func set_animation():
+	if movement.x < 0:
+		$Sprite.flip_h = true
+		$AnimationPlayer.play("move")
+	
+	if movement.x > 0:
+		$Sprite.flip_h = false
+		$AnimationPlayer.play("move")
+	
+	if movement.x == 0:
+		$AnimationPlayer.play("idle")
